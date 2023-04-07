c186c894ae19cb5d5b3d6fe82eaf38b02cb3b78f
Tyler Ferreira <tyferreira4@hotmail.com>
2023-04-07 03:13:22 -0400

    Add playlist and searchresults components

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TrackList from "../TrackList/TrackList";
+import "./SearchResults.css";
+
+function SearchResults() {
+    return (
+        <div className="SearchResults">
+            <h2>Results</h2>
+            {/* <!-- Add a TrackList component --> */}
+        </div>
+    );
+}
+
+export default SearchResults;
